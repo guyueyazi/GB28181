15fb2d6e2ac2735b0b4be2e04a1efb84a1d51fdb
liyq <liyuanquan@qiniu.com>
2020-06-01 19:14:55 +0800

    update

--- src/gb28181.c
+++ src/gb28181.c
@@ -376,6 +376,10 @@ int sip_event_handle(eXosip_event_t *evtp)
                 invite_ack_handle(evtp);
             }
             break;
+        case EXOSIP_REGISTRATION_FAILURE:
+            dbg_dump_response("evtp");
+            LOGI("EXOSIP_REGISTRATION_FAILURE");
+            break;
         default:
             LOGI("msg type: %d", evtp->type);
             break;
